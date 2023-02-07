7503a2cad7e45c2a51fcfe313f9035c107a3b7ee
LiavB2 <liavbar5@gmail.com>
2023-02-07 14:01:19 -0800

    add athlete copies

---
+++ api/athletecopy.py
@@ -0,0 +1,68 @@
+from flask import Blueprint, request, jsonify
+from flask_restful import Api, Resource, reqparse # used for REST API building
+from __init__ import db
+from model.athletescopy import Athlete
+
+athlete_api = Blueprint('athlete_api', __name__,
+                   url_prefix='/api/athlete')
+
+api = Api(athlete_api)
+
+class AthleteAPI(Resource):
+    class _Create(Resource):
+        def post(self):
+            ''' Read data for json body '''
+            body = request.get_json()
+            
+            Age = body.get('Age')
+            Weight = body.get('Weight')
+            Bench = body.get('Bench')
+            Squat = body.get('Squat')
+            Pullup= body.get('Pullup')
+            Mile = body.get('Mile')
+
+            ''' #1: Key code block, setup USER OBJECT '''
+            uo = Athlete(Age, Weight, Bench, Squat, Pullup, Mile)
+            
+            ''' #2: Key Code block to add user to database '''
+            # create user in database
+            user = uo.create()
+            # success returns json of user
+            if user:
+                return jsonify(user.read())
+            # failure returns error
+            return {'message': f'Something went wrong (error)'}, 210
+    # resources
+   # class _Create(Resource):
+    #    def post(self):
+     #       parser = reqparse.RequestParser()
+      #      parser.add_argument("Weight", required=False, type=int)
+       #     parser.add_argument("Bench", required=False, type=int)
+        #    parser.add_argument("Squat", required=False, type=int)
+         #   parser.add_argument("Press", required=False, type=int)
+          #  parser.add_argument("Pushup", required=False, type=int)
+           # args = parser.parse_args()
+
+            #athlete = Athlete(args["Weight"], args["Bench"], args["Squat"], args["Press"], args["Pushup"])
+            #try:
+             #   db.session.add(athlete)
+              #  db.session.commit()
+               # return athlete.to_dict(), 201
+            #except Exception as e:
+             #   db.session.rollback()
+              #  return {"message": f"server error: {e}"}, 500
+
+    class _Read(Resource):
+        def get(self):
+            athletes = Athlete.query.all()    # read/extract all users from database
+            json_ready = [ath.read() for ath in athletes]  # prepare output in json
+            return jsonify(json_ready)  # jsonify creates Flask response object, more specific to APIs than json.dumps
+
+
+
+
+    api.add_resource(_Read, "/")
+    api.add_resource(_Create, "/create")
+
+
+
---
+++ model/athletescopy.py
@@ -0,0 +1,167 @@
+import json
+
+from __init__ import app, db
+from sqlalchemy.exc import IntegrityError
+
+class Athlete(db.Model):
+    __tablename__= "Ath"
+   
+    id = db.Column(db.Integer, primary_key=True)
+    _Age = db.Column(db.Integer, nullable=False)
+    _Weight = db.Column(db.Integer, nullable=False)
+    _Bench = db.Column(db.Integer, nullable=False)
+    _Squat = db.Column(db.Integer, nullable=False)
+    _Pullup = db.Column(db.Integer, nullable=False)
+    _Mile = db.Column(db.Integer, nullable=False)
+   
+    def __init__(self, Age, Weight, Bench, Squat, Pullup, Mile):
+        self._Age = Age
+        self._Weight = Weight
+        self._Bench = Bench
+        self._Squat = Squat
+        self._Pullup = Pullup
+        self._Mile = Mile
+
+    def __repr__(self):
+        return "<Athlete(Age='%s', Weight='%s', Bench='%s', Squat='%s', Pullup='%s', Mile='%s')>" % (
+            self._Age,
+            self._Weight,
+            self._Bench,
+            self._Squat,
+            self._Pullup,
+            self._Mile
+        )
+    
+    # Getters:
+    @property
+    def Age(self):
+        return self._Age
+    
+    @property
+    def Weight(self):
+        return self._Weight
+    
+    @property
+    def Bench(self):
+        return self._Bench
+
+    @property
+    def Squat(self):
+        return self._Squat   
+
+    @property
+    def Pullup(self):
+        return self._Pullup 
+    
+    @property
+    def Mile(self):
+        return self._Mile 
+   
+    # Setters    
+    @Age.setter
+    def Age(self, age):
+        self._Age = age
+
+    @Weight.setter
+    def Weight(self,weight):
+        self._Weight = weight
+
+    @Bench.setter
+    def Bench(self, bench):
+        self._Bench = bench
+
+    @Squat.setter
+    def Squat(self, squat):
+        self._Squat = squat
+
+    @Pullup.setter
+    def Pullup(self, pullup):
+        self._Press = pullup
+
+    @Mile.setter
+    def Mile(self, mile):
+        self.Mile = mile
+
+    @property
+    def dictionary(self):
+        dict = {
+            "Age" : self.Age,
+            "Weight" : self.Weight,
+            "Bench" : self.Bench,
+            "Squat" : self.Squat,
+            "Pullup" : self.Pullup,
+            "Mile" : self.Mile,
+        }
+        return dict
+
+    def __str__(self):
+        return json.dumps(self.dictionary)
+    
+    def __repr__(self):
+        return f'Athlete(Age={self._Age}, Weight={self._Weight}, Bench={self._Bench}, Squat={self._Squat}, Pullup={self._Pullup}, Mile={self._Mile})'
+
+    def create(self):
+        try:
+            # creates a person object from User(db.Model) class, passes initializers
+            db.session.add(self)  # add prepares to persist person object to Users table
+            db.session.commit()  # SqlAlchemy "unit of work pattern" requires a manual commit
+            print("commit")
+            return self
+        except IntegrityError:
+            print("issue")
+            db.session.remove()
+            return None
+
+    def read(self):
+        return {
+            "Age" : self.Age,
+            "Weight" : self.Weight,
+            "Bench" : self.Bench,
+            "Squat" : self.Squat,
+            "Pullup" : self.Pullup,
+            "Mile" : self.Mile
+        }
+
+    # CRUD update:
+    # returns self
+    def update(self, Age="", Weight="", Bench="", Squat="", Pullup="", Mile=""):
+        """only updates values with length"""
+        if len(Age) > 0:
+            self.Age = Age
+        if len(Weight) > 0:
+            self.Weight = Weight
+        if len(Bench) > 0:
+            self.Bench = Bench
+        if len(Squat) > 0:
+            self.Squat(Squat)
+        if len(Pullup) > 0:
+            self.Pullup(Pullup)
+        if len(Mile) > 0:
+            self.Mile(Mile)
+        db.session.commit()
+        return self
+
+    # CRUD delete: remove self
+    # None
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        return None
+
+
+def initAthletescopy():
+    db.create_all()
+    """Tester data for table"""
+    Liav = Athlete(16, 130, 180, 260, 12, 6.42)
+    Noor = Athlete(17, 190, 240, 380, 15, 7.35)
+
+
+    Athletes = [Liav, Noor]
+
+    for athlete in Athletes:
+        try:
+            print("h")
+            athlete.create()
+        except IntegrityError:
+            db.session.remove()
+            print(f"Records, exist, duplicate data, or error: {athlete.id}")
